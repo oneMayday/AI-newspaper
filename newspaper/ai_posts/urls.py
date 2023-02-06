307af49a9f4a4a690c3218ebf72dfd93dbb06868
Sazonov Pavel <pps.mayday@gmail.com>
2023-02-07 00:12:43 +0400

    structure update

--- newspaper/ai_posts/urls.py
+++ newspaper/ai_posts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('contacts/', contacts, name='contacts'),
     path('about/', about, name='about'),
     path('mailing/', mailing, name='mailing'),
+    # path('categories/<')
 ]
