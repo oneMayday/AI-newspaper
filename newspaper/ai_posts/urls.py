f9c0b9b5bbecadc4592f420322a43d79752a4696
Sazonov Pavel <pps.mayday@gmail.com>
2023-02-09 15:23:55 +0400

    merge bugs fixed

--- newspaper/ai_posts/urls.py
+++ newspaper/ai_posts/urls.py
@@ -7,13 +7,6 @@ urlpatterns = [
     path('contacts/', contacts, name='contacts'),
     path('about/', about, name='about'),
     path('mailing/', mailing, name='mailing'),
-<<<<<<< HEAD
-    path('categories/', show_categories, name='categories'),
-    path('categories/<int:category_id>/', show_cat_posts, name='cat_posts'),
-    # path('categories/<int:category_id/post/<int:post_id>', show_post, name='post'),
-
-=======
     path('categories/', categories, name='categories'),
     path('categories/<int:cat_id>/', all_posts, name='posts'),
->>>>>>> feat/post-temp
 ]
--- newspaper/ai_posts/views.py
+++ newspaper/ai_posts/views.py
@@ -22,27 +22,12 @@ def about(request):
     return render(request, 'ai_posts/about.html', {'title': 'О нас'})
 
 
-<<<<<<< HEAD
-def show_categories(request):
-=======
 def categories(request):
->>>>>>> feat/post-temp
     cats = Category.objects.all()
     return render(request, 'ai_posts/categories.html', {'categories': cats})
 
 
-<<<<<<< HEAD
-def show_cat_posts(request, category_id):
-    category = Category.objects.filter(pk=category_id)[0].title
-    posts = Post.objects.filter(post_category_id=category_id)
-    return render(request, 'ai_posts/all_posts.html', {'posts': posts, 'category': category})
-
-
-def show_post(request):
-    pass
-=======
 def all_posts(request, cat_id):
     category = Category.objects.filter(pk=cat_id)[0]
     posts = Post.objects.filter(post_category_id=cat_id)
     return render(request, 'ai_posts/all_posts.html', {'category': category, 'posts': posts})
->>>>>>> feat/post-temp
